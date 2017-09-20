083315f648729c9dae0d24de2edc7a1b3a20ce1b
Sam Tyler <sam@samtyler.com.au>
2017-09-20 10:50:44 +0930

    Get beer categories

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
-console.log("JS Loaded");
+var API_URL = '/api/';
+
+var categories = [];
+var beers = [];
+
+function buildCategoriesMenu() {
+  for (i in categories) {
+    var cat = categories[i];
+
+    var label = $("<label>", {
+      for: "cat" + cat["id"]
+    }).text(cat["name"]);
+
+    var checkbox = $("<input>", {
+      type: "checkbox",
+      id: "cat" + cat["id"],
+      value: cat["id"],
+      class: "category",
+      click: updateBeers
+    })
+
+    $("#category").append(checkbox);
+    $("#category").append(label);
+  }
+}
+
+function getCategories() {
+  $.get(API_URL + 'categories', function(result) {
+    categories = result['data'];
+    buildCategoriesMenu();
+  })
+}
+
+function updateBeers() {
+  console.log("Updating Beers");
+}
+
+getCategories();
+$(document).ready(buildCategoriesMenu);
