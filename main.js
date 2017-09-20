0ae6d1f86e5f56ba19c621dfa20c596fbd7efac6
Sam Tyler <sam@samtyler.com.au>
2017-09-20 11:34:18 +0930

    Add sliders for alcohol content

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ var API_URL = '/api/';
 
 var categories = [];
 var beers = [];
+var min_abv = 4;
+var max_abv = 7;
+var min_ibu = 12;
+var max_ibu = 22;
 
 function buildCategoriesMenu() {
   for (i in categories) {
@@ -35,5 +39,34 @@ function updateBeers() {
   console.log("Updating Beers");
 }
 
+function createSliders() {
+  $("#abv").slider({
+    range: true,
+    min: 3,
+    max: 8,
+    values: [min_abv, max_abv],
+    slide: function(event, ui) {
+      min_abv = ui.values[0];
+      max_abv = ui.values[1];
+      updateBeers();
+    }
+  });
+  $("#ibu").slider({
+    range: true,
+    min: 11,
+    max: 23,
+    values: [min_ibu, max_ibu],
+    slide: function(event, ui) {
+      min_abv = ui.values[0];
+      max_abv = ui.values[1];
+      updateBeers();
+    }
+  });
+}
+
 getCategories();
-$(document).ready(buildCategoriesMenu);
+$(document).ready(function() {
+    createSliders();
+    buildCategoriesMenu();
+  }
+);
